d91891561d501bb3c4c49daf12a1121fbd4a7c86
Anton Volchkov <anton.volchkov.2001@gmail.com>
2021-09-18 01:53:41 +0300

    update

---
+++ TestNotification/Notification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TestNotification
+{
+    public interface INotification
+    {
+         long Id { get; set; }
+    }
+
+    public class Notification : INotification
+    {
+        public long Id { get; set; }
+    }
+}
--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using MassTransit;
 using TestNotification;
 
+
 namespace WebApplication1.Controllers
 {
     [ApiController]
